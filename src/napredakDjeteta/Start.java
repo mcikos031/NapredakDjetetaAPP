46ea4212305bb2cd6ea66cb3648b82eb23753c38
mcikos031 <martin.cikos0607@gmail.com>
2018-03-24 12:58:04 +0100

    Inicijalno

---
+++ src/napredakDjeteta/Start.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package napredakDjeteta;
+
+;
+
+
+/**
+ *
+ * @author Admin
+ */
+public class Start {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        
+        //
+        
+//        Obrada<Smjer> o = new Obrada<>();
+//        
+//        Smjer s = new Smjer();
+//        s.setNaziv("J17");
+//        s.setCijena(BigDecimal.TEN);
+//        
+//        o.save(s);
+
+//        ObradaPolaznik op = new ObradaPolaznik();
+//        Polaznik p = new Polaznik();
+//        
+//        try {
+//            op.spremi(p);
+//        } catch (EdunovaException ex) {
+//            System.out.println(ex.getMessage());
+//        }
+//        
+//        Obrada<Polaznik> obrada = new Obrada<>();
+//        obrada.save(p);
+       
+    }
+    
+}
---
+++ src/napredakDjeteta/model/Profesor.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package napredakDjeteta.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Polaznik
+ */
+
+public class Profesor implements Serializable {
+ 
+    public Profesor(){
+    super();
+    }
+    
+    public Profesor(String ime, String prezime){
+        
+    }
+    
+}
